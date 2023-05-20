d0398e91de5ef8decadf69929f13945ec3fe506d
MazayKun <kir99916@gmail.com>
2023-05-20 16:56:03 +0300

    Залил ДЗ

---
+++ src/main/java/ru/mikheev/kirill/hw01gradle/HelloOtus.java
@@ -0,0 +1,16 @@
+package ru.mikheev.kirill.hw01gradle;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        var animalsList = List.of("Кошка", "Собака", "Суслослон");
+        StringJoiner stringJoiner = new StringJoiner(", ", "Список животных: ", "!");
+        for(var animal : animalsList) {
+            stringJoiner.add(animal);
+        }
+        System.out.println(stringJoiner);
+    }
+}
